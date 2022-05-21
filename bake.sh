68769ffbbbdd6e0c3e2a1387183942d58e02f1e4
saintabomination <saint.abomination@protonmail.com>
2022-05-21 10:18:08 +0200

    Initial Commit

---
+++ bake.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PROJECT_NAME="sfml-platformer"
+COMPILER="g++"
+SRC_DIR="src"
+BUILD_DIR="build"
+
+if [[ $1 == "" ]]
+then
+    $COMPILER -c $SRC_DIR/Main.cpp
+    mkdir -p $BUILD_DIR
+    $COMPILER *.o -o $BUILD_DIR/$PROJECT_NAME
+    rm *.o
+fi
